7bf50a9e429ec93280225c3c2392440ea38454a8
Sander Joos <sander.joos@skynet.be>
2016-12-17 17:15:50 +0100

    finished centralservice

--- DataService/src/main/java/rest/ApplicationConfig.java
+++ DataService/src/main/java/rest/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(rest.HumHistoryREST.class);
+        resources.add(rest.MeasurementREST.class);
         resources.add(rest.TempHistoryREST.class);
     }
     
--- DataService/src/main/java/service/CentralService.java
+++ DataService/src/main/java/service/CentralService.java
@@ -6,6 +6,9 @@
 package service;
 
 import entities.Measurement;
+import gatherers.MeasurementGatherer;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 
@@ -19,12 +22,40 @@ public class CentralService {
     @Inject
     public MeasurementDBService service;
     
+    @Inject
+    public MeasurementGatherer gatherer;
+    
     public void storeMeasurement(Measurement measurement){
         service.storeMeasurement(measurement);
     }
     
     public List<Measurement> getAllMeasurements(){
         return service.getAllMeasurements();
+    }
+    
+    public List<Measurement> getAllMeasurementsForDate(LocalDate date){
+        List<Measurement> result = new ArrayList<>();
+        for(Measurement m : this.getAllMeasurements()){
+            if(m.getDate().equals(date)){
+                result.add(m);
+            }
+        }
+        return result;
+    }
+    
+    public List<Measurement> getAllMeasurementsAfterDate(LocalDate date){
+        List<Measurement> result = new ArrayList<>();
+        for(Measurement m : this.getAllMeasurements()){
+            if(m.getDate().isAfter(date)){
+                result.add(m);
+            }
+        }
+        return result;
+    }
+    
+    public void retrieveNewDate(){
+        Measurement m = this.gatherer.getMeasurement();
+        this.service.deleteMeasurement(m);
     }
     
 }
